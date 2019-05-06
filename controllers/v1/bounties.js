9330b5860a2274881b3efde3ed002f34e1af313b
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-05-06 16:17:19 -0700

    more stubbed out bounty routes

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -1,7 +1,31 @@
 let express = require('express');
 
+// router instance
 let router = express.Router();
 
+// GET /bounties
 router.get('/', (req, res) => {
   res.send('stub');
 });
+
+// POST /bounties
+router.post('/', (req, res) => {
+  res.send('stub')
+})
+
+// GET /bounties/:id
+router.get('/', (req, res) => {
+  res.send('stub')
+})
+
+// PUT /bounties/:id
+router.put('/', (req, res) => {
+  res.send('stub')
+})
+
+// DELETE /bounties/:id
+router.delete('/', (req, res) => {
+  res.send('stub')
+})
+
+module.exports = router;
